3f378a58fac104f56fdb5515609ad5e6c0487a0e
Luke-Gilbert <69307250+Luke-Gilbert@users.noreply.github.com>
2021-02-24 11:32:26 +0000

    Update 3)angle_to_pwm_example.py

--- Examples/3)angle_to_pwm_example.py
+++ Examples/3)angle_to_pwm_example.py
@@ -1,6 +1,6 @@
 #Required Code
 # 1. import servo_angles                       #Imports Module
-# 2. Servo = servo_angles.servo                #Call the class variable anything you want but adapt the bellow building blocks
+# 2. Servo = servo_angles.servo()                #Call the class variable anything you want but adapt the bellow building blocks
 #'Do the Action' Code                       #Bellow assumes you called the class variable (Required 2.) to Servo. If you did not: Adapt the bollow where says Servo.the_feature. 
 # 1. Servo.angle(angle, pin number)         #Sets the servo angle to the first positional argument(angle), for the pin number in the seccond positional argument (pin number) ** See Bottom
 # 2. A_Variable = (Servo.convert_pwm(90))   #(Servo.convert_pwm(angle)) returns the required Pwm value to reach the required variable.
